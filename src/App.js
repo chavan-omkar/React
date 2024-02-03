23c5ec084ec72d7b8d0a2a1ad9bc0ed2f8e52230
OmkarChavan98 <omkar.chavan@jungleegames.com>
2024-02-03 23:00:17 +0530

    added comments on state props useeffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+const { useState, useEffect } = require("react");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ export default function App(){
+
+  /*
+  this are the states used so runtime availability of the data 
+  advice and count are used to get while setAdvice and setCount is used to set the values of the state
+  */
+  const[advice,setAdvice] = useState("")
+  const[count,setCount] = useState(0);
+
+  async function getAdvice(){
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount(c => c + 1);
+  }
+
+  // The empty array [] means that the effect does not depend on any values from the component scope.
+  useEffect(function(){
+    getAdvice();
+  },[]);
+
+  /*
+   <Message count ={count} here we have used props.
+   we are passing the count as a property  of props to message component.
+   */
+  return(
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count ={count} />
     </div>
   );
 }
 
-export default App;
+function Message(props){
+ return (
+   <p>You have read <strong>{props.count}</strong> piece of advice already</p>
+ );
+}
